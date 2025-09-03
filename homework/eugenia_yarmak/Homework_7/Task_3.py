90caccaa0c63b631987c5e552c470bd9a6555553
Eugenia <eugenia.pecevich@gmail.com>
2025-09-03 13:39:35 +0300

    Homework_7 done

---
+++ homework/eugenia_yarmak/Homework_7/Task_3.py
@@ -0,0 +1,8 @@
+def result_line (line):
+    get_num = int(line.split()[-1]) + 10
+    return get_num
+
+print(result_line("результат операции: 42"))
+print(result_line("результат операции: 54"))
+print(result_line("результат работы программы: 209"))
+print(result_line("результат: 42"))
