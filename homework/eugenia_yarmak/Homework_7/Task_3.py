270ca01f08bcc8ae89c6a6c821b8de6898c17b9f
Eugenia <eugenia.pecevich@gmail.com>
2025-09-03 13:51:04 +0300

    Homework_7 fixed

--- homework/eugenia_yarmak/Homework_7/Task_3.py
+++ homework/eugenia_yarmak/Homework_7/Task_3.py
@@ -1,6 +1,7 @@
-def result_line (line):
+def result_line(line):
     get_num = int(line.split()[-1]) + 10
     return get_num
+
 
 print(result_line("результат операции: 42"))
 print(result_line("результат операции: 54"))
